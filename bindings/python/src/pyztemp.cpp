6e0bc0981fe243f2b746bcf9280e8629e5c56141
Sylvain Garcia <sylvain.garcia@cyberio-dsi.com>
2015-12-21 23:24:47 +0100

    Some progress on python bindings.

--- bindings/python/src/pyztemp.cpp
+++ bindings/python/src/pyztemp.cpp
@@ -1,5 +1,5 @@
-//#include <boost/python.hpp>
-/*
+#include <boost/python.hpp>
+
 namespace py = boost::python;
 char const* render(const std::string & input, const py::dict & context)
 {
@@ -10,4 +10,4 @@ BOOST_PYTHON_MODULE(pyztemp)
 {
     py::def("render", render,
             ( py::arg("input"), py::arg("context")));
-}*/
+}
